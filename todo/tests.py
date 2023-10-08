bd024c339f54b8ac86bf7d0d2c56969e8aa82b18
Kuldeep Pisda <kuldeep.pisda@tejgyan.org>
2023-10-08 07:25:39 -0400

    Added FirstToDoListTestCase Test Solution

--- todo/tests.py
+++ todo/tests.py
@@ -1,11 +1,17 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase  # noqa F401
+
+from todo.lists.models import List
+
+User = get_user_model()
 
 
 class FirstToDoListTestCase(TestCase):
     def setUp(self):
-        # TODO: Create a TODO List and a USer
-        pass
+        user = User.objects.create(username="testuser", password="12345")
+        List.objects.create(name="testlist", user=user)
 
     def test_todo_list(self):
-        # TODO: Test that the todo list is created and the values are correct
-        pass
+        list = List.objects.get(name="testlist")
+        self.assertEquals(list.name, "testlist")
+        self.assertEquals(list.user.username, "testuser")
