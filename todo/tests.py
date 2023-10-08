e85c8a1f6a3c489fb28b5367ac96e18f97803fdb
Kuldeep Pisda <kuldeep.pisda@tejgyan.org>
2023-10-08 07:21:24 -0400

    Added FirstToDoListTestCase class

--- todo/tests.py
+++ todo/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase  # noqa F401
 
-# Create your tests here.
+
+class FirstToDoListTestCase(TestCase):
+    def setUp(self):
+        # TODO: Create a TODO List and a USer
+        pass
+
+    def test_todo_list(self):
+        # TODO: Test that the todo list is created and the values are correct
+        pass
